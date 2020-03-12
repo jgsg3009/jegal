ab2efb1bf59671dd7721917e6aca41dcdc722c7f
jgsg3009 <jgsg3009@innorules.com>
2020-03-12 14:59:27 +0900

    200320 3시 commit

---
+++ study/element-ui/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+        <el-row>
+            <el-col :span="24">
+                <div class="grid-content bg-purple-dark"></div>
+            </el-col>
+        </el-row>   
+    </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+    name : 'app',
+    components : {
+        HelloWorld
+    }
+}
+</script>
+
+<style>
+
+#app {
+    font-family: 'Avenir', Helvetica,Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+}
+
+</style>

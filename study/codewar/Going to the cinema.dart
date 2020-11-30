a16e63b9c054285f688c805f32f476b7916b0b74
jgsg3009 <jgsg3009@innorules.com>
2020-12-01 08:25:00 +0900

    ee

--- study/codewar/Going to the cinema.dart
+++ study/codewar/Going to the cinema.dart
@@ -3,12 +3,14 @@ import 'dart:math'
 int movie(int card, int ticket, double perc) {
   
   int n = (card / ticket).ceil();
-  int ticketPrice = card + (ticket*perc*(1-perc^n)/()).ceil();
+  int ticketPrice = card + (ticket*perc*(1-perc.pow(n))/(1-perc)).ceil();
   int nonTicketPrice = ticket*n;
+  int diff = ticketPrice - nonTicketPrice
+
   do {
     
 
-  } while(n > 1);
+  } while(diff > 0);
 
   return n;
 }

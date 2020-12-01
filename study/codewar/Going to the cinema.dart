ddad56ac10a710bee370d2fbae5e48e811d4c956
rpa-agent <rpa-agent@innorules.com>
2020-12-01 21:48:33 +0900

    ee

--- study/codewar/Going to the cinema.dart
+++ study/codewar/Going to the cinema.dart
@@ -1,18 +1,30 @@
-import 'dart:math'
+import 'dart:math';
 
 int movie(int card, int ticket, double perc) {
-  
   int n = (card / ticket).ceil();
-  int ticketPrice = card + (ticket*perc*(1-perc.pow(n))/(1-perc)).ceil();
-  int nonTicketPrice = ticket*n;
-  int diff = ticketPrice - nonTicketPrice
-
+  double perPowN = pow(perc, n);
+  int cardPrice = card + (ticket * perc * (1 - perPowN) / (1 - perc)).ceil();
+  int nonCardPrice = ticket * n;
+  int diff = cardPrice - nonCardPrice;
   do {
-    
-
-  } while(diff > 0);
-
+    int stepN = (diff / ticket).ceil();
+    n += stepN;
+    perPowN *= pow(perc, stepN);
+    cardPrice = card + (ticket * perc * (1 - perPowN) / (1 - perc)).ceil();
+    nonCardPrice = ticket * n;
+    diff = cardPrice - nonCardPrice;
+    if (diff == 0) {
+      n += 1;
+      perPowN *= perc;
+    }
+  } while (diff >= 0);
   return n;
 }
 
-void main() {}
+void main() {
+  print(movie(500, 15, 0.9));
+  print(movie(100, 10, 0.95));
+  print(movie(0, 10, 0.95));
+  print(movie(250, 20, 0.9));
+  print(movie(500, 20, 0.9));
+}
